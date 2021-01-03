a8d15c18116839d6164735858855ae926c6da69e
oliverandrews <jaysonphillipmitchell@gmail.com>
2021-01-03 17:19:44 -0500

    It works now.

--- main.ts
+++ main.ts
@@ -1,60 +1,54 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin, PluginSettingTab, Setting, TAbstractFile, TFile } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface IIrisSettings {
+	header: string;
+	location: string,
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: IIrisSettings = {
+	header: '### Tasks',
+	location: "Daily Notes"
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: IIrisSettings;
+
+	private _files: Array<TFile>;
+	private _dailyNoteLocation: string;
 
 	async onload() {
-		console.log('loading plugin');
-
+		console.log('IRIS!');
+		this.addSettingTab(new IrisSettings(this.app, this));
 		await this.loadSettings();
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
-
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
+		this._dailyNoteLocation = this.settings.location;
+		this.app.workspace.on("layout-ready", () => {
+			this._files = Array.from(this.app.vault
+				.getFiles()
+				.filter((entry) => entry.path.includes(this._dailyNoteLocation)));
+			console.log(this._files);
+			this.app.vault.on("create", async (file) => {
+				const heading = this.settings.header;
+				if (file.path.includes(this._dailyNoteLocation)) {
+					const thisFileDate = this.parseDate(file.name);
+					let outcome = this._files
+						.map((existingFile) => thisFileDate - this.parseDate(existingFile.name))
+					const min = outcome.reduce((min, val) => (min > val) ? min : val);
+					const preceeding = this._files[outcome.indexOf(min)];
+					const textPrevious = await this.app.vault.read(preceeding);
+					let text = await this.app.vault.read(this.app.workspace.getActiveFile());
+					const unfinishedTodosRegex = /- \[ \].*/g
+					const unfinishedTodos = Array.from(textPrevious.matchAll(unfinishedTodosRegex)).map((entry) => entry[0]);
+					console.log(`${heading}${unfinishedTodos.join('\n')}\n`);
+					text = text.replace(heading, `${heading}\n${unfinishedTodos.join('\n')}`);
+					console.log(text);
+					await this.app.vault.modify(this.app.workspace.getActiveFile(), text);
 				}
-				return false;
-			}
+			})
 		});
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-		console.log('unloading plugin');
+	parseDate(name: string) {
+		return (new Date(name.replace(".md", ""))).getTime()
 	}
 
 	async loadSettings() {
@@ -66,47 +60,47 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class IrisSettings extends PluginSettingTab {
 	plugin: MyPlugin;
+	private notes: string;
+	private tasks: string;
 
 	constructor(app: App, plugin: MyPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
+		this.notes = "";
+		this.tasks = "";
 	}
 
 	display(): void {
-		let {containerEl} = this;
+		let { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for Iris' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('To Do Headding')
+			.setDesc('Enter the tag above your to-do list. (IE: ### To Do).')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
+				.setPlaceholder('### To Do')
+				.setValue(this.plugin.settings.header ? this.plugin.settings.header : DEFAULT_SETTINGS.header)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.header = value;
+					this.tasks = value;
 					await this.plugin.saveSettings();
 				}));
+		new Setting(containerEl)
+			.setName('Daily Note Directory')
+			.setDesc('Enter the directory you keep your Daily Notes in.')
+			.addText(text => text
+				.setPlaceholder('Daily Notes')
+				.setValue(this.plugin.settings.location ? this.plugin.settings.location : DEFAULT_SETTINGS.location)
+				.onChange(async (value) => {
+					this.plugin.settings.location = value;
+					this.notes = value;
+					await this.plugin.saveSettings();
+				}));
+			
 	}
 }
